5fa1a5e9209f64f53d78a558f4b563c5568c55f7
2036 <2036@users.noreply.github.com>
2022-10-06 21:23:06 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #下载核心程序
-mkdir /v2ray
-cd /v2ray
-rm -f /v2ray/v2ray
-wget https://github.com/teasiu/goorm-v2ray/raw/master/v2ray
+mkdir /caddy
+cd /caddy
+rm -f /caddy/caddy
+wget https://github.com/2036/goorm/raw/master/caddy
 #设置运行权限
-chmod +x /v2ray/v2ray
-rm -f /v2ray/v2ctl
-wget https://github.com/teasiu/goorm-v2ray/raw/master/v2ctl
-chmod +x /v2ray/v2ctl
-rm -f /v2ray/config.json
-wget https://github.com/teasiu/goorm-v2ray/raw/master/config.json
+chmod +x /caddy/caddy
+rm -f /caddy/config.json
+wget https://github.com/2036/goorm/raw/master/config.json
 # 开始后台运行
-nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
+nohup /caddy/caddy -config=/caddy/config.json >out.txt 2>&1 &
